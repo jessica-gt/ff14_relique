d69334e4c48f1ea0ca7684e45f765ba2f9dd7d68
jessica-gt <58854245+jessica-gt@users.noreply.github.com>
2024-04-19 14:10:50 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,67 @@
+function affichage(x,y){
+	if (x==1){
+		document.getElementById("btnPrecedent").disabled = true;
+		document.getElementById("btnSuivant").disabled = false;
+	}else if (x==y) {
+		document.getElementById("btnPrecedent").disabled = false;
+		document.getElementById("btnSuivant").disabled = true;
+	}else{
+		document.getElementById("btnPrecedent").disabled = false;
+		document.getElementById("btnSuivant").disabled = false;
+	}
+	for (let i = 1; i <= y; i++) {
+  		if (i==x) {
+  			document.getElementById("etape"+i).style.display = "block";
+  		}else {
+  			document.getElementById("etape"+i).style.display = "none";
+  		};
+	};	
+};
+
+function arme(x){
+	document.getElementById("imgRelic").innerHTML='<img src="../image/image_reliques/'+x+'/50(1).png">';
+};
+
+function precedent(nbPage){
+	var test = (document.getElementById("etape1").style.display == "block");
+	for (let i = 1; i <= nbPage; i++){
+		test = (document.getElementById("etape"+i).style.display == "block");
+		if (test) {
+			if (i==2){
+				document.getElementById("etape"+i).style.display = "none";
+				document.getElementById("etape"+(i-1)).style.display = "block";
+				document.getElementById("btnPrecedent").disabled = true;
+			}else if(i==8){
+				document.getElementById("etape"+i).style.display = "none";
+				document.getElementById("etape"+(i-1)).style.display = "block";
+				document.getElementById("btnSuivant").disabled = false;
+			}else{
+				document.getElementById("etape"+i).style.display = "none";
+				document.getElementById("etape"+(i-1)).style.display = "block";
+				document.getElementById("btnPrecedent").disabled = false;
+			}
+		}
+	}
+}
+
+function suivant(nbPage){
+	var test = (document.getElementById("etape1").style.display == "block");
+	for (let i = nbPage; i >= 1; i--){
+		test = (document.getElementById("etape"+i).style.display == "block");
+		if (test) {
+			if (i==(nbPage-1)){
+				document.getElementById("etape"+i).style.display = "none";
+				document.getElementById("etape"+(i+1)).style.display = "block";
+				document.getElementById("btnSuivant").disabled = true;
+			}else if(i==1){
+				document.getElementById("etape"+i).style.display = "none";
+				document.getElementById("etape"+(i+1)).style.display = "block";
+				document.getElementById("btnPrecedent").disabled = false;
+			}else{
+				document.getElementById("etape"+i).style.display = "none";
+				document.getElementById("etape"+(i+1)).style.display = "block";
+				document.getElementById("btnSuivant").disabled = false;
+			}
+		}
+	}
+}
