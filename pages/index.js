7a587c90e5ce8b5c7e8c684d45842acf1e5ba1d9
kirara <58854245+jessica-gt@users.noreply.github.com>
2024-12-14 16:22:35 +0100

    amélioration du site et ajout des reliques 90

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function affichage(x,y){
-	if (x==1){
+	if (x==0){
 		document.getElementById("btnPrecedent").disabled = true;
 		document.getElementById("btnSuivant").disabled = false;
 	}else if (x==y) {
@@ -9,11 +9,13 @@ function affichage(x,y){
 		document.getElementById("btnPrecedent").disabled = false;
 		document.getElementById("btnSuivant").disabled = false;
 	}
-	for (let i = 1; i <= y; i++) {
+	for (let i = 0; i <= y; i++) {
   		if (i==x) {
   			document.getElementById("etape"+i).style.display = "block";
+  			document.getElementById("bouton"+i).disabled = true;
   		}else {
   			document.getElementById("etape"+i).style.display = "none";
+  			document.getElementById("bouton"+i).disabled = false;
   		};
 	};	
 };
@@ -26,12 +28,14 @@ function precedent(nbPage){
 	var test = (document.getElementById("etape1").style.display == "block");
 	for (let i = 1; i <= nbPage; i++){
 		test = (document.getElementById("etape"+i).style.display == "block");
+		document.getElementById("bouton"+i).disabled = false;
 		if (test) {
-			if (i==2){
+			document.getElementById("bouton"+(i-1)).disabled = true;
+			if (i==1){
 				document.getElementById("etape"+i).style.display = "none";
 				document.getElementById("etape"+(i-1)).style.display = "block";
 				document.getElementById("btnPrecedent").disabled = true;
-			}else if(i==8){
+			}else if(i==nbPage){
 				document.getElementById("etape"+i).style.display = "none";
 				document.getElementById("etape"+(i-1)).style.display = "block";
 				document.getElementById("btnSuivant").disabled = false;
@@ -46,14 +50,16 @@ function precedent(nbPage){
 
 function suivant(nbPage){
 	var test = (document.getElementById("etape1").style.display == "block");
-	for (let i = nbPage; i >= 1; i--){
+	for (let i = nbPage; i >= 0; i--){
 		test = (document.getElementById("etape"+i).style.display == "block");
+		document.getElementById("bouton"+i).disabled = false;
 		if (test) {
+			document.getElementById("bouton"+(i+1)).disabled = true;
 			if (i==(nbPage-1)){
 				document.getElementById("etape"+i).style.display = "none";
 				document.getElementById("etape"+(i+1)).style.display = "block";
 				document.getElementById("btnSuivant").disabled = true;
-			}else if(i==1){
+			}else if(i==0){
 				document.getElementById("etape"+i).style.display = "none";
 				document.getElementById("etape"+(i+1)).style.display = "block";
 				document.getElementById("btnPrecedent").disabled = false;
